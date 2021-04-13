d9a7f5ff4307692d85178f96640e1d5086a94bea
raza-n <tatairazan@gmail.com>
2021-04-13 12:52:31 -0400

    Settings Fragment Completed

--- app/src/main/java/com/example/finalproject/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/finalproject/fragments/SettingsFragment.java
@@ -1,66 +1,93 @@
 package com.example.finalproject.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.finalproject.LoginActivity;
 import com.example.finalproject.R;
+import com.parse.ParseUser;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String TAG = "SettingFragment";
+
+
+    private EditText etNewPassword;
+    private EditText etNewPassword2;
+
+    protected Button btnChangePassword;
+    protected Button btnLogout;
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        etNewPassword = view.findViewById(R.id.etNewPassword);
+        etNewPassword2 = view.findViewById(R.id.etNewPassword2);
+
+        btnChangePassword = view.findViewById(R.id.btnChangePassword);
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String newPassword = etNewPassword.getText().toString();
+                String newPassword2 = etNewPassword2.getText().toString();
+
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                if (!newPassword.equals(newPassword2)){
+                    Toast.makeText(getActivity(),"Passwords do not match!",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    currentUser.setPassword(newPassword);
+                    currentUser.saveInBackground();
+                    Toast.makeText(getActivity(),"Password has successfully been changed!",Toast.LENGTH_SHORT).show();
+                    etNewPassword. setText("");
+                    etNewPassword2. setText("");
+                }
+            }
+        });
+
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                goLoginActivity();
+            }
+        });
+
+    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(getContext(), LoginActivity.class);
+        startActivity(i);
     }
 }
