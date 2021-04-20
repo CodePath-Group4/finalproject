68dab3a25c012b0a339a91c214f5084790bcb632
bsc24 <bchin08@gmail.com>
2021-04-20 17:11:45 -0400

    Added registers for Song and Favorite models.

--- app/src/main/java/com/example/finalproject/ParseApplication.java
+++ app/src/main/java/com/example/finalproject/ParseApplication.java
@@ -2,7 +2,10 @@ package com.example.finalproject;
 
 import android.app.Application;
 
+import com.example.finalproject.models.Favorite;
+import com.example.finalproject.models.Song;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -25,6 +28,10 @@ public class ParseApplication extends Application {
 
         // set applicationId, and server server based on the values in the back4app settings.
         // any network interceptors must be added with the Configuration Builder given this syntax
+
+        ParseObject.registerSubclass(Song.class);
+        ParseObject.registerSubclass(Favorite.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("gPkAymJm0pyYC8S9HJlroVf84cjrdvf1IbYs8K7l") // should correspond to Application Id env variable
                 .clientKey("2wUG1kUDDYs9oNswbOabOU03v2wzSMknZaJpeDxA")  // should correspond to Client key env variable
