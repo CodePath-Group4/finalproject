e0a010028093429d1d4dad4faf50d1c0fb1d55f5
bsc24 <bchin08@gmail.com>
2021-04-20 21:18:33 -0400

    Added click listeners for like and favorite buttons.

--- app/src/main/java/com/example/finalproject/SongsAdapter.java
+++ app/src/main/java/com/example/finalproject/SongsAdapter.java
@@ -1,9 +1,11 @@
 package com.example.finalproject;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -52,6 +54,10 @@ public class SongsAdapter extends RecyclerView.Adapter<SongsAdapter.ViewHolder> 
         private TextView tvSongName;
         private TextView tvArtistName;
         private TextView tvAlbumName;
+        private Button btnLike;
+        private Button btnFavorite;
+        boolean liked;
+        boolean favorited;
 
 
         public ViewHolder(@NonNull View itemView) {
@@ -61,28 +67,55 @@ public class SongsAdapter extends RecyclerView.Adapter<SongsAdapter.ViewHolder> 
             tvSongName = itemView.findViewById(R.id.tvSongName);
             tvArtistName = itemView.findViewById(R.id.tvArtistName);
             tvAlbumName = itemView.findViewById(R.id.tvAlbumName);
+            btnLike = itemView.findViewById(R.id.btnLike);
+            btnFavorite = itemView.findViewById(R.id.btnFavorite);
+
+            liked = false;
+            favorited = false;
         }
 
         public void bind(Song song) {
 
-            String url = "wmhbradio.org/wp-content/uploads/2016/07/music-placeholder.png";
-
-
             Glide.with(context)
-//                    .load(song.getArtUrl());
-                    .load("https://i.scdn.co/image/ab67616d0000b2731ae2bdc1378da1b440e1f610")
+                    .load(song.getImageUrl())
+//                    .load("https://i.scdn.co/image/ab67616d0000b2731ae2bdc1378da1b440e1f610")
                     .fitCenter()
                     .into(ivAlbumArt);
 
             //TODO: Display correct information from each song
-            //tvSongName.setText(song.getName());
-            tvSongName.setText("FILLER SONG NAME");
+            tvSongName.setText(song.getSongName());
+//            tvSongName.setText("FILLER SONG NAME");
 
-            //tvArtistName.setText(song.getArtist());
-            tvArtistName.setText("FILLER ARTIST NAME");
+            tvArtistName.setText(song.getArtistName());
+//            tvArtistName.setText("FILLER ARTIST NAME");
 
-            //tvAlbumName.setText(song.getAlbum());
-            tvAlbumName.setText("FILLER ALBUM NAME");
+            tvAlbumName.setText(song.getAlbumName());
+//            tvAlbumName.setText("FILLER ALBUM NAME");
+
+
+            btnLike.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i(TAG, "ivLike, onClick");
+                    liked = !liked;
+                    if (liked)
+                        btnLike.setBackgroundResource(R.drawable.ufi_heart_active);
+                    else
+                        btnLike.setBackgroundResource(R.drawable.ufi_heart);
+                }
+            });
+
+            btnFavorite.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i(TAG, "ivFavorite, onClick");
+                    favorited = !favorited;
+                    if (favorited)
+                        btnFavorite.setBackgroundResource(R.drawable.ufi_save_active);
+                    else
+                        btnFavorite.setBackgroundResource(R.drawable.ufi_save);
+                }
+            });
         }
     }
 }
